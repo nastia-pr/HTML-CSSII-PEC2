53d9814f285a1b79bc532bf1606db8ecbeed640e
nastia-pr <apronyuk@uoc.edu>
2023-12-06 21:27:27 +0100

    Changes

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,8 +14,7 @@ import mapboxgl from 'mapbox-gl';
  * Write any other JavaScript below
  */
 
-    mapboxgl.accessToken =
-        "pk.eyJ1IjoibmFzbmFzdGkiLCJhIjoiY2xwczl3d3V3MDFoZTJqcXM5N3NremZmZiJ9.7SlUptC06BFl3rrYk3jYtQ";
+mapboxgl.accessToken = "pk.eyJ1IjoibmFzbmFzdGkiLCJhIjoiY2xwczl3d3V3MDFoZTJqcXM5N3NremZmZiJ9.7SlUptC06BFl3rrYk3jYtQ";
 
 const santiagoDeCompostelaCoordinates = [-8.5449, 42.8782];
 
